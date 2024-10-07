4443beb42f0f51452e41cca52d1bd14e0161dd62
Minsu3703 <nmlkj123@naver.com>
2024-10-07 17:30:43 +0900

    사운드 추가
    - 로비 UI 사운드 추가
    - 볼륨 셋팅 오디오소스 연동

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class GameManager : NetworkBehaviour
 {
+    [Header("# UI")]
     [SerializeField] private AimController aimController;
     [SerializeField] private List<Image> skillUI;
     [SerializeField] private Image hpBar;
 
+    [Header("# Audio")]
+    [SerializeField] private AudioSource bgAudioSource;
+
     public AimController AimController { get { return aimController; } }
     public List<Image> SkillUI { get { return skillUI; } }
     public Image HpBar { get { return hpBar; } }
+    public AudioSource BgAudioSource { get { return bgAudioSource; } }
 }
---
+++ Assets/_Scripts/Lobby/VolumeSettingManager.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSettingManager : MonoBehaviour
+{
+    [Header("# Sound bar Settings")]
+    [SerializeField] private Slider bgBar;
+    [SerializeField] private Slider skillBar;
+    [SerializeField] private Slider uiBar;
+    [SerializeField] private Slider voiceBar;
+
+    [Header("# Audio Sources")]
+    [SerializeField] private AudioSource bgAudioSource;
+    [SerializeField] private AudioSource skillAudioSource;
+    [SerializeField] private AudioSource uiAudioSource;
+    [SerializeField] private AudioSource voiceAudioSource;
+
+    private float bgVolume = 1f;
+    private float skillVolume = 1f;
+    private float uiVolume = 1f;
+    private float voiceVolume = 1f;
+
+
+    private void Awake()
+    {
+        if (FindAnyObjectByType<VolumeSettingManager>() != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+
+        InitVolumeOnLobbyScene();
+    }
+
+    // Will be used on first line of Awake
+    public void InitVolumeSettings()
+    {
+        bgBar.value = bgVolume;
+        skillBar.value = skillVolume;
+        uiBar.value = uiVolume;
+        voiceBar.value = voiceVolume;
+    }
+
+    public void InitVolumeOnLobbyScene()
+    {
+        LobbyManager lobbyManager = FindAnyObjectByType<LobbyManager>();
+
+        bgAudioSource = lobbyManager.BgAudioSource;
+        uiAudioSource = lobbyManager.UiAudioSource;
+
+        bgAudioSource.volume = bgVolume;
+        uiAudioSource.volume = uiVolume;
+
+        InitVolumeSettings();
+    }
+
+    public void InitVolumeOnGameScene()
+    {
+        GameManager gameManager = FindAnyObjectByType<GameManager>();
+
+        bgAudioSource = gameManager.BgAudioSource;
+        // Todo: skillAudioSource를 PlayerData에서 받아와야 함
+        // Todo: voiceAudioSource를 PlayerData에서 받아와야 함
+
+        bgAudioSource.volume = bgVolume;
+        skillAudioSource.volume = skillVolume;
+        voiceAudioSource.volume = voiceVolume;
+    }
+
+    public void SetBackgroundVolume()
+    {
+        bgAudioSource.volume = bgBar.value;
+        bgVolume = bgBar.value;
+    }
+
+    public void SetSkillVolume()
+    {
+        //skillAudioSource.volume = skillBar.value;
+        skillVolume = skillBar.value;
+    }
+
+    public void SetUIVolume()
+    {
+        uiAudioSource.volume = uiBar.value;
+        uiVolume = uiBar.value;
+    }
+
+    public void SetVoiceVolume()
+    {
+        //voiceAudioSource.volume = voiceBar.value;
+        voiceVolume = voiceBar.value;
+    }
+}
